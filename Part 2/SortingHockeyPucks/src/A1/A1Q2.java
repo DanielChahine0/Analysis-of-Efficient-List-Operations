ab4b78d85f018d4dca10777afef35367e342782a
DanielChahine0 <chahined@my.yorku.ca>
2024-01-30 21:20:39 -0500

    Update A1Q2.java

--- Part 2/SortingHockeyPucks/src/A1/A1Q2.java
+++ Part 2/SortingHockeyPucks/src/A1/A1Q2.java
@@ -1,5 +1,59 @@
+/**
+ * EECS 2101 N, Winter 2024
+ * Assignment 1, Question 2 starter code.
+ * Please read carefully the instructions in the assignment handout
+ * and the starter code.
+ */
 package A1;
+// Do NOT modify the package declaration
+
+import java.util.ArrayList;
+// Do NOT add any import
+
+// You may implement a new class here, such as a Stack or a Queue.
+// Any additional class that you use must be included in THIS file.
+class 
 
 public class A1Q2 {
 
+    /**
+     * The function that you need to implement.
+     * Read the assignment handout for the specification.
+     *
+     * DO NOT MODIFY the declaration of the function (its parameter types and return type).
+     */
+    public static int solve(int[] arr) {
+    	
+    	int numberOfPucks = 0;
+    	
+    	for 
+    	
+    	
+        return 0;
+    }
+
+    /**
+     * This main function contains a few test cases that can be used to check
+     * that your code compiles and runs. Note that these test cases are NOT complete,
+     * and you need to test your code thoroughly with more cases.
+     */
+    public static void main(String[] args) {
+
+        // Printing "true" means the return value is correct.
+
+        int[] arr = {4, 5, 2, 1, 3};
+        System.out.println(3 == solve(arr));
+
+        arr = new int[] {5, 4, 3, 1, 2};
+        System.out.println(5 == solve(arr));
+
+        arr = new int[] {1};
+        System.out.println(1 == solve(arr));
+
+        arr = new int[] {2, 3, 1};
+        System.out.println(1 == solve(arr));
+
+        arr = new int[] {7, 1, 5, 4, 3, 2, 8, 10, 9, 6};
+        System.out.println(6 == solve(arr));
+    }
 }
